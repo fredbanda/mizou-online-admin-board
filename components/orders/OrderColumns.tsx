d7f28d45daf67f0dc6d4bad6c88c23e0c7904649
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2025-01-19 20:53:26 +0200

    mizou-online admin

---
+++ components/orders/OrderColumns.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+
+export const columns: ColumnDef<OrderColumnType>[] = [
+  {
+    accessorKey: "_id",
+    header: "Order",
+    cell: ({ row }) => {
+      return (
+        <Link
+          href={`/orders/${row.original._id}`}
+          className="hover:to-blue-600 hover:underline"
+        >
+          {row.original._id}
+        </Link>
+      );
+    },
+  },
+  {
+    accessorKey: "customer",
+    header: "Customer",
+  },
+  {
+    accessorKey: "products",
+    header: "Products",
+  },
+  {
+    accessorKey: "totalAmount",
+    header: "Total (ZAR)",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created At",
+  },
+];
